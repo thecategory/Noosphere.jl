7c2ad7cef1df77c89be91e705f56cb6d5f24c044
ddyok <dk@thecategory.com>
2021-08-20 15:07:15 +0200

    Some sort of plot.

--- Noosphere.jl
+++ Noosphere.jl
@@ -4,11 +4,34 @@ using CSV
 using DataFrames
 using Missings
 using URIs
-using Plots
-# using StatsPlots
-using UnicodePlots
+using PlotlyJS
+using DataFrames
 
 uri = "https://global-mind.org/cgi-bin/eggdatareq.pl"
+
+mutable struct Params
+  z::Int
+  year::Int64
+  month::Int64
+  day::Int64
+  stime::String
+  etime::String
+  gzip::String
+  idate::String
+end
+
+mutable struct Header
+  samples_per_record::Int64
+  seconds_per_record::Int64
+  records_per_packet::Int64
+  trial_size::Int64
+  eggs_reporting::Int64
+  start_time::String
+  end_time::String
+  seconds_of_data::Int64
+  
+  Header() = new()
+end
 
 params = Dict(
     "z" => "1",
@@ -16,7 +39,7 @@ params = Dict(
     "month" => "8",
     "day" => "1",
     "stime" => "00:00:00",
-    "etime" => "00:01:00",
+    "etime" => "00:10:00",
     "gzip" =>"No",
     "idate"=>"Yes"
 )
@@ -33,9 +56,11 @@ function getparams(params)
 end
 
 function get()
-    uri_withparam = URI(uri * getparams(params))
+    uri_withparam = uri * getparams(params)
+    println("getting " * uri_withparam)
     r = HTTP.get(uri_withparam)
     str = decode(r.body, enc"ASCII")
+    savetofile(str)
     return str
 end
 
@@ -51,36 +76,74 @@ function splitheader(str)
   return spl
 end
 
-function getheaderdf(str, spl)
-  faux_cols = "c1, c2, c3, c4\n"
-  headerstr = faux_cols * str[1:spl[1] - 5]
-  headerdf = dropmissing(CSV.File(IOBuffer(headerstr), silencewarnings=true) |> DataFrame)
-  return headerdf
+# 10,1,10,"Samples per record"
+# 10,2,10,"Seconds per record"
+# 10,3,30,"Records per packet"
+# 10,4,200,"Trial size"
+# 11,1,21,"Eggs reporting"
+# 11,2,1627776000,"Start time",2021-08-01 00:00:00
+# 11,3,1627776060,"End time",2021-08-01 00:01:00
+# 11,4,61,"Seconds of data"
+
+function getheader(str, spl)  
+  headerstr = str[1:spl[1] - 5]
+  spl = split(headerstr, ",")
+  println(spl)
+  header = Header()
+  header.samples_per_record =  parse(Int64, strip(String(spl[3]), "\\n"))
+  header.seconds_per_record = parse(Int64, strip(String(spl[7]), "\\n"))
+  header.records_per_packet = parse(Int64, spl[11])
+  header.trial_size = parse(Int64, spl[15])
+  header.eggs_reporting = parse(Int64, spl[19])
+  header.start_time = spl[23]
+  header.end_time = spl[28]
+  return header
 end
 
 function getdf(str, spl)
   datastr = str[spl[1] - 1:length(str)]
-  df = dropmissing(CSV.File(IOBuffer(datastr), silencewarnings=true) |> DataFrame)
+  df = CSV.File(IOBuffer(datastr), silencewarnings=true) |> DataFrame
+  println(df)
   return df
 end
 
 function test()
   str = get()
   spl = splitheader(str)
-  headerdf = getheaderdf(str, spl)
-  println(headerdf)
+  # header = getheader(str, spl)
+  # println(header)
   df = getdf(str, spl)
-  println(df)
+  for col in names(df)
+    df[col] = Missings.coalesce.(df[col], 0)
+  end
+
+  data = AbstractTrace[]
+  for col in names(df)[2:end]
+    if col == "Column2"
+      continue
+    end
+    push!(data, scatter(x=:gmtime, y=df[col], mode="lines", name=col))
+  end
+
+  layout = Layout(title="Egg Data",  
+                width=1000,
+                height=700)
+
+  p = plot(data, layout)
+
+  savefig(p, "p.html")
+
 end
 
 function testplot()
-  df = CSV.read("data.csv", DataFrame)
-
-  unicodeplots()
-  plot(df.gmtime, [df[:2], df[:3], df[:4], df[:5]])
+  #names!(df, Symbol.(replace.(string.(names(df)), Ref(r"\s"=>""))))
+  # df = CSV.read("data.csv", DataFrame)
+  # println(df)
+  # unicodeplots()
+  # plot(df.gmtime, [df[2:end]])
    # savefig(p, "data.png")
 end
 
-testplot()
+#testplot()
 
-# test()
+test()
