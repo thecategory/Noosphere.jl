38f9688b6f3914e73a9b1ac0285745bf1b5a01a5
ddyok <dk@thecategory.com>
2021-08-21 19:49:52 +0200

    Parsing header and more info in plot.

--- Noosphere.jl
+++ Noosphere.jl
@@ -7,6 +7,8 @@ using URIs
 using PlotlyJS
 using DataFrames
 using StatsBase
+using DelimitedFiles
+using Dates
 
 uri = "https://global-mind.org/cgi-bin/eggdatareq.pl"
 
@@ -27,8 +29,8 @@ mutable struct Header
   records_per_packet::Int64
   trial_size::Int64
   eggs_reporting::Int64
-  start_time::String
-  end_time::String
+  start_time::Int64
+  end_time::Int64
   seconds_of_data::Int64
   
   Header() = new()
@@ -66,7 +68,6 @@ function get(params)
 end
 
 function savetofile(str)
-    # println(str)
     f = open("data.csv", "w")
     write(f, str)
     close(f)
@@ -83,21 +84,42 @@ end
 # 10,4,200,"Trial size"
 # 11,1,21,"Eggs reporting"
 # 11,2,1627776000,"Start time",2021-08-01 00:00:00
-# 11,3,1627776060,"End time",2021-08-01 00:01:00
-# 11,4,61,"Seconds of data"
+# 11,3,1627776600,"End time",2021-08-01 00:10:00
+# 11,4,601,"Seconds of data"
 
 function getheader(str, spl)  
   headerstr = str[1:spl[1] - 5]
-  spl = split(headerstr, ",")
-  # println(spl)
+
   header = Header()
-  header.samples_per_record =  parse(Int64, strip(String(spl[3]), "\\n"))
-  header.seconds_per_record = parse(Int64, strip(String(spl[7]), "\\n"))
-  header.records_per_packet = parse(Int64, spl[11])
-  header.trial_size = parse(Int64, spl[15])
-  header.eggs_reporting = parse(Int64, spl[19])
-  header.start_time = spl[23]
-  header.end_time = spl[28]
+  i = 1
+  for line in eachline(IOBuffer(headerstr))
+    val = readdlm(IOBuffer(line), ',')[:3]
+
+    if i == 1
+      header.samples_per_record =  val
+    elseif i == 2
+      header.seconds_per_record = val
+    elseif i == 3
+      header.records_per_packet = val
+    elseif i == 4
+      header.trial_size = val
+    elseif i == 5
+      header.eggs_reporting = val
+    elseif i == 6
+      header.start_time = val
+    elseif i == 7
+      header.end_time = val
+    elseif i == 8
+      header.seconds_of_data = val
+    else
+      println("error")
+    end
+    i = i + 1
+  end
+
+  # println(header)
+
+
   return header
 end
 
@@ -119,6 +141,7 @@ end
 function test()
   str = get(params)
   spl = splitheader(str)
+  header = getheader(str, spl)
 
   df = getdf(str, spl)
   for col in names(df)
@@ -132,9 +155,11 @@ function test()
   end
 
   s = scatter(x=:gmtime, y=res, mode="lines")
+
   layout = Layout(title="Egg Data (Root Mean Square) for " * 
-                params["year"] * "/" * params["month"] * "/" * params["day"] * " " *
-                params["stime"] * " - " * params["etime"],  
+                Dates.format(Dates.unix2datetime(header.start_time), "yyyy-mm-dd HH:MM:SS") * " - " *
+                Dates.format(Dates.unix2datetime(header.end_time), "yyyy-mm-dd HH:MM:SS") * " (" *
+                string(header.eggs_reporting) * " Eggs Reporting)", 
                 width=1000,
                 height=700)
 
