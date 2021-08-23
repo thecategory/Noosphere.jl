4c2ee3ed80cedb5b0411c20752936d99665b946c
ddyok <dk@thecategory.com>
2021-08-23 18:55:17 +0200

    Initial support for multi day fetch.

--- src/Noosphere.jl
+++ src/Noosphere.jl
@@ -11,26 +11,41 @@ using Missings
 using Dates
 using TranscodingStreams
 using CodecZlib
+# using MarketData
+
+include("src/util.jl")
 
 REQ_URI = "https://global-mind.org/cgi-bin/eggdatareq.pl"
 
-export Params
+mutable struct Duration
+    start_day::Date
+    end_day::Date
+end
 
 mutable struct Params
     year::Int64
     month::Int64
     day::Int64
-    stime::String #Dates.Time
-    etime::String #Dates.Time
-    gzip::Bool
+    stime::String # Dates.Time
+    etime::String # Dates.Time
+
+    multiday::Bool
+    duration::Duration
 end
 
-# default values of 10 minute egg data
-function Params()
-    # stime = Dates.Time(t -> Dates.minute(t) == 00, 00)
-    # etime = Dates.Time(t -> Dates.minute(t) == 10, 00)
+# function DefParams(;year::Int64=2018,month::Int64=1,day::Int64=1,stime::String="00:00:00",etime::String="00:00:00",multiday::Bool=false,duration::Duration=Duration(Date(2018,8,1), Date(2018,8,3)))
+function Params(;kwargs...)  
+  kwargs_dict = Dict(kwargs)
 
-    Params(2021, 8, 1, "00:00:00", "00:10:00", true)
+  if haskey(kwargs_dict, :duration) == true
+    Params(0, 0, 0, 
+           "", "", 
+           true, kwargs[:duration])
+  else
+    Params(kwargs[:year],kwargs[:month],kwargs[:day],
+          kwargs[:stime],kwargs[:etime],
+          false, Duration(Date(1970,1,1), Date(1970,1,1)))
+  end
 end
 
 mutable struct Header
@@ -46,44 +61,70 @@ mutable struct Header
     Header() = new()
 end
 
-mutable struct Results
+mutable struct Result
     header::Header
     data::DataFrame
 end
 
+function getduration(dur)
+    start_day = dur.start_day
+    end_day = dur.end_day
+
+    duration = start_day:Day(1):end_day
+
+    return collect(duration)
+end
+
+# function getrequestparams(params)
+#   return getrequestparams(params.year, params.month, params.day, params.stime, params.etime)
+# end
+
 function getrequestparams(params)
+  return getrequestparams(params.year, params.month, params.day, params.stime, params.etime)
+end
+
+function getrequestparams(year, month, day, stime, etime)
     "?z=1" *
-    "&year=" * string(params.year) *
-    "&month=" * string(params.month) *
-    "&day=" * string(params.day) *
-    "&stime=" * params.stime * # Dates.format(params.stime, "HH:MM:SS") *
-    "&etime=" * params.etime * #Dates.format(params.etime, "HH:MM:SS") *
-    "&gzip=" * (params.gzip ? "Yes" : "No") *
+    "&year=" * string(year) *
+    "&month=" * string(month) *
+    "&day=" * string(day) *
+    "&stime=" * stime * # Dates.format(params.stime, "HH:MM:SS") *
+    "&etime=" * etime * # Dates.format(params.etime, "HH:MM:SS") *
+    "&gzip=Yes" * # (params.gzip ? "Yes" : "No") *
     "&idate=No"
 end
 
 function get(params)
-    uri_withparam = REQ_URI * getrequestparams(params)
-    println("getting " * uri_withparam)
-    r = HTTP.get(uri_withparam)
+    if params.multiday == true
+        dur = getduration(params.duration)
+        results = Result[]
 
-    str = ""
-    if params.gzip == true
-      io = TranscodingStream(GzipDecompressor(), IOBuffer(r.body))
-      str = read(io, String)
+        for day in dur
+            req = getrequestparams(Dates.year(day), Dates.month(day), Dates.day(day), 
+                                  "00:00:00", "23.59.59")
+            uri_withparam = REQ_URI * req
+            println("getting " * uri_withparam)
+            ret = httpgetgzip(uri_withparam)
+            res = Result(ret)
+            push!(results, res)
+        end
+
+        return results
     else
-      str = decode(r.body, enc"ASCII")
+        uri_withparam = REQ_URI * getrequestparams(params)
+        println("getting " * uri_withparam)
+        ret = httpgetgzip(uri_withparam)
+        return Result(ret)
     end
 
-    #savetofile(str)
 
-    return Results(str)
-end
+    # else
+    #   str = decode(r.body, enc"ASCII")
+    # end
 
-function savetofile(str)
-    f = open("out/data.csv", "w")
-    write(f, str)
-    close(f)
+    # savetofile(str)
+
+
 end
 
 function splitheader(str)
@@ -101,17 +142,17 @@ end
 # 11,4,601,"Seconds of data"
 
 function getheader(str, spl)  
-  headerstr = str[1:spl[1] - 5]
+    headerstr = str[1:spl[1] - 5]
 
-  header = Header()
-  i = 1
+    header = Header()
+    i = 1
 
-  for line in eachline(IOBuffer(headerstr))
-    val = readdlm(IOBuffer(line), ',')[:3]
+    for line in eachline(IOBuffer(headerstr))
+        val = readdlm(IOBuffer(line), ',')[:3]
 
     if i == 1
       header.samples_per_record =  val
-    elseif i == 2
+        elseif i == 2
       header.seconds_per_record = val
     elseif i == 3
       header.records_per_packet = val
@@ -126,67 +167,72 @@ function getheader(str, spl)
     elseif i == 8
       header.seconds_of_data = val
     end
-    i = i + 1
-  end
+        i = i + 1
+    end
 
-  return header
+    return header
 end
 
 function getdataframe(str, spl)
-  datastr = str[spl[1] - 1:length(str)]
-  df = CSV.File(IOBuffer(datastr), silencewarnings=true) |> DataFrame
-  for col in names(df)
-    df[col] = Missings.coalesce.(df[col], 0)
-  end
+    datastr = str[spl[1] - 1:length(str)]
+    df = CSV.File(IOBuffer(datastr), silencewarnings=true) |> DataFrame
+    for col in names(df)
+        df[col] = Missings.coalesce.(df[col], 0)
+    end
 
-  return df
-end
-
-function rootmeansquare(A)
-  s = 0.0
-  for a in A
-     s += a * a
-  end
-  return sqrt(s / length(A))
+    return df
 end
 
 function saveplot(res)
-  header = res.header
-  df = res.data
+    header = res.header
+    df = res.data
 
-  res = []
+    res = []
 
-  for row in eachrow(df[3:end])
-    push!(res, rootmeansquare(Array(row)))
-  end
+    for row in eachrow(df[3:end])
+        push!(res, rootmeansquare(Array(row)))
+    end
 
-  s = scatter(x=:gmtime, y=res, mode="lines")
+    s = scatter(x=:gmtime, y=res, mode="lines")
 
-  layout = Layout(title="Egg Data (Root Mean Square) for " * 
+    layout = Layout(title="Egg Data (Root Mean Square) for " *
                   Dates.format(header.start_time, "yyyy-mm-dd HH:MM:SS") * " - " *
-                  Dates.format(header.end_time, "yyyy-mm-dd HH:MM:SS") *
-                  " (" * string(header.eggs_reporting) * " Eggs Reporting)", 
+                  Dates.format(header.end_time, "HH:MM:SS") *
+                  " (" * string(header.eggs_reporting) * " Eggs Reporting)",
                   width=1200,
                   height=700)
 
-  p = plot(s, layout)
+    p = plot(s, layout)
 
-  savefig(p, "out/p.html")
+    savefig(p, "out/p.html")
 end
 
-function Results(str)
-  spl = splitheader(str)
-  header = getheader(str, spl)
-  df = getdataframe(str, spl)
-  Results(header, df)
+function Result(str)
+    spl = splitheader(str)
+    header = getheader(str, spl)
+    df = getdataframe(str, spl)
+    Result(header, df)
 end
 
 function test()
-  params = Params(2021, 8, 1, "00:00:00", "00:10:00", true)
-  
-  res = get(params)
+    # start_day = Date(2021, 8, 1)
+    # end_day = Date(2021, 8, 3)
+    # dur = Duration(start_day, end_day)
 
-  saveplot(res)
+    # params = Params(duration=dur)
+    # res = get(params)
+
+
+    params = Params(year=2021, month=8, day=1, stime="00:00:00", etime="23:59:59")
+    res = get(params)
+    saveplot(res)
+
+    # start = DateTime(2021, 8, 1)
+    # YahooOpt(period1 = start)
+
+    # df = yahoo("^DJI") |> DataFrame
+    # println(df)
+
 end
 
 test()
---
+++ src/util.jl
@@ -0,0 +1,48 @@
+macro optstruct(s)
+  if s.head !== :struct 
+      error("Not a struct def")
+  end 
+
+  ismutable = s.args[1]
+  if !ismutable
+      error("Not mutable")
+  end
+  
+  name = s.args[2]
+  body = s.args[3]    
+
+  ctor = :(function $(name)(;kwargs...)
+              K = new()
+              for (key, value) in kwargs
+                  field_type_key = typeof(getfield(K,key))
+                  setfield!(K, key, convert(field_type_key, value))
+              end
+              return K
+          end)
+          
+  newbody = [body.args; ctor]
+  
+  return Expr(s.head, ismutable, name, Expr(body.head, newbody...))       
+end
+
+function rootmeansquare(A)
+  s = 0.0
+  for a in A
+     s += a * a
+  end
+  return sqrt(s / length(A))
+end
+
+function httpgetgzip(uri)
+  r = HTTP.get(uri)
+  io = TranscodingStream(GzipDecompressor(), IOBuffer(r.body))
+  str = read(io, String)
+
+  return str
+end
+
+function savetofile(str)
+  f = open("out/data.csv", "w")
+  write(f, str)
+  close(f)
+end
